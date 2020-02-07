a21b107559e2d2acc1e43921911c91426fea4cd8
madolphe <madolphe@ensc.fr>
2020-02-07 16:47:52 +0100

    infinite bouncing balls add to the app

--- interface_experimentation/static/js/test.js
+++ interface_experimentation/static/js/test.js
@@ -10,6 +10,11 @@ function setup() {
   bug2 = new Jitter();
   bug3 = new Jitter();
   bug4 = new Jitter();
+  bug5 = new Jitter();
+  bug6 = new Jitter();
+  bug7 = new Jitter();
+  bug8 = new Jitter();
+
 }
 
 function draw() {
@@ -22,6 +27,11 @@ function draw() {
   bug3.display();
   bug4.move();
   bug4.display();
+  bug5.move();
+  bug5.display();
+  bug6.move();
+  bug6.display();
+  bug7.move();
 }
 
 // Jitter class
@@ -29,40 +39,54 @@ class Jitter {
   constructor() {
     this.x = random(width);
     this.y = random(height);
-    this.diameter = random(10, 30);
-    this.dir = Math.floor(random(-4,4)+1);
-    console.log(width)
-    console.log(height)
-
+    this.diameter = random(20, 50);
+    this.dir = Math.floor(random(0,7)+1);
+    this.x_speed = 3;
+    this.y_speed = 3;
   }
   move() {
-    switch (this.dir) {
-        case 1:
-            this.x +=1;
-        case 2:
-            this.x +=1;
-            this.y +=1;
-        case 3:
-            this.y += 1;
-        case 4:
-            this.x -=1;
-            this.y +=1;
-        case 5:
-            this.x -= 1;
-        case 6:
-            this.x -=1;
-            this.y -=1;
-        case 7:
-            this.y -=1;
-        case 8:
-            this.y -=1;
-            this.x += 1;
+    if (this.x > width) {
+        this.x = width ;
+        this.x_speed = -1*this.x_speed;
     }
-    if (this.x>width || this.x <0 || this.y>height || this.y<0){
-        this.dir = Math.floor(random(1,8)+1);
+    else if (this.x < 0) {
+        this.x = 0;
+        this.x_speed = -1*this.x_speed;
+    }
+    else if (this.y > height) {
+        this.y = height;
+        this.y_speed = -1*this.y_speed;
+    }
+    else if (this.y < 0) {
+        this.y = 0;
+        this.y_speed = -1*this.y_speed;
+    }
+    else{
+        switch (this.dir)
+        {
+            case 0:
+                this.x -=  this.x_speed;
+            case 1:
+                this.x -=  this.x_speed;
+                this.y -= this.y_speed;
+            case 2:
+                this.y -= this.y_speed;
+            case 3:
+                this.x +=  this.x_speed;
+                this.y -= this.y_speed;
+            case 4:
+                this.x +=  this.x_speed;
+            case 5:
+                this.x +=  this.x_speed;
+                this.y += this.y_speed;
+            case 6:
+                this.y += this.y_speed;
+            case 7:
+                this.y += this.y_speed;
+                this.x -=  this.x_speed;
+        }
     }
   }
-
   display() {
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
