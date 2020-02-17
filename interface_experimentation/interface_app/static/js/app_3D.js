81cd682a37d8f5fdbf77087f1bf1955b9cdfa3af
madolphe <madolphe@ensc.fr>
2020-02-17 17:23:34 +0100

    ball is moving but doesn't fit with the screen's dimension, has to be fixed

--- interface_experimentation/interface_app/static/js/app_3D.js
+++ interface_experimentation/interface_app/static/js/app_3D.js
@@ -4,10 +4,13 @@ let windowDepth = 800;
 let ball;
 
 class Ball{
-    constructor(color, radius_max){
-        this.x = random(-windowWidth,windowWidth);
-        this.y = random(-windowHeight, windowHeight);
-        this.z = random(-windowDepth, windowDepth);
+    constructor(color, radius_max, speed){
+        this.x = random(-windowWidth/2,windowWidth/2);
+        this.y = random(-windowHeight/2, windowHeight/2);
+        this.z = random(-windowDepth/2, windowDepth/2);
+        this.speedx = speed;
+        this.speedy = speed;
+        this.speedz = speed;
         console.log(this.x, this.y, this.z);
         this.radius = random(radius_max);
         this.color = color;
@@ -20,6 +23,15 @@ class Ball{
         // 24 is recommended by the docs (number of polygons for 3D)
         sphere(this.radius, 24, 24);
         pop();
+    }
+    move(){
+        // Screen doesnt fit with the movement!
+        if((this.x > windowHeight/2) || (this.x < -windowHeight/2)){this.speedx=-this.speedx}
+        if((this.y > windowWidth/2) || (this.y < -windowWidth/2)){this.speedy=-this.speedy}
+        if((this.z > windowDepth/2) || (this.z < -windowDepth/2)){this.speedz=-this.speedz}
+        this.x = this.x + this.speedx;
+        this.y = this.y + this.speedy;
+        this.z = this.z + this.speedz;
     }
 }
 
@@ -49,7 +61,7 @@ class App{
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  ball = new Ball('yellow', 100, 24, 24);
+  ball = new Ball('yellow', 100, 1);
 
 }
 
@@ -57,5 +69,7 @@ function draw() {
     background(100);
     noFill();
     box(windowWidth, windowHeight, windowDepth);
+    ball.move();
     ball.display();
+
 }
