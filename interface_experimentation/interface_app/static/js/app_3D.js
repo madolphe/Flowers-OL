5822ba688ffe739228dee82e320675ab6ec674a1
madolphe <madolphe@ensc.fr>
2020-02-21 16:49:56 +0100

    add colisions

--- interface_experimentation/interface_app/static/js/app_3D.js
+++ interface_experimentation/interface_app/static/js/app_3D.js
@@ -1,3 +1,8 @@
+// @TODO: add app.timer to share all parts of the experiment
+// @TODO: add clickable state of balls so that user can respond
+// @TODO: add json reader and launch the parameterized app from the outside
+
+
 let windowWidth = 1000;
 let windowHeight = 600;
 let windowDepth = 800;
@@ -13,17 +18,36 @@ class Ball{
         this.speedx = speed;
         this.speedy = speed;
         this.speedz = speed;
-        console.log(this.x, this.y, this.z);
         this.radius = random(50, radius_max);
+        this.square_size = this.radius*sqrt(2)/2;
+        this.square_size=this.radius/2;
         this.color = color;
         this.display();
-        this.contact = function(ball)
-        {
-            if((this.x == ball.x) && (this.y == ball.y) && (this.z == ball.z))
-            {
-                // Do something when there is a collision 
-            }
-        }
+    }
+    contact(ball){
+         let x1 = this.x - this.square_size;
+         let x2 = this.x + this.square_size;
+         let y1 = this.y + this.square_size;
+         let y2 = this.y + this.square_size;
+         let z1 = this.z + this.square_size;
+         let z2 = this.z + this.square_size;
+         if((((x1 - (ball.x - this.square_size)) > 0) && ((x1 - (ball.x + this.square_size))<0))||
+             (((x2 - (ball.x - this.square_size)) > 0) && ((x2 - (ball.x + this.square_size))<0))){
+             // balls are aligned along x-axis, let's check y-axis:
+             if((((y1 - (ball.y - this.square_size)) > 0) && ((y1 - (ball.y + this.square_size))<0))||
+             (((y2 - (ball.y - this.square_size)) > 0) && ((y2 - (ball.y + this.square_size))<0))){
+                // balls are aligned along y-axis, let's check z-axis:
+                 if((((z1 - (ball.z - this.square_size)) > 0) && ((z1 - (ball.z + this.square_size))<0))||
+                    (((z2 - (ball.z - this.square_size)) > 0) && ((z2 - (ball.z + this.square_size))<0))){
+                     ball.speedx = -ball.speedx;
+                     ball.speedy = -ball.speedy;
+                     ball.speedz = -ball.speedz;
+                     this.speedx = -this.speedx;
+                     this.speedy = -this.speedy;
+                     this.speedz = -this.speedz;
+                 }
+             }
+         }
     }
     display(){
         push();
@@ -50,38 +74,42 @@ class App{
         this.n_distractors = n_distractors;
         this.targets = [];
         this.distractors = [];
-        this.frozen = true;
+        this.frozen = false;
         for(let step = 0; step < this.n_targets; step++){
-            this.targets.push(new Ball(target_color, 80, 3))
+            this.targets.push(new Ball(target_color, 40, 3))
         }
         for(let step = 0; step < this.n_distractors; step++){
-            this.distractors.push(new Ball(distractor_color, 80, 3))
+            this.distractors.push(new Ball(distractor_color, 40, 3))
         }
-        this.display_balls = function(){
-        // functions used to display all balls
-        this.targets.forEach(function (item, index, array){item.display()});
-        this.distractors.forEach(function (item, index, array){item.display()})
-        };
-        this.move_balls = function(){
-            // functions used to display all balls
-            if (!this.frozen){
-                this.targets.forEach(function (item, index, array){item.move()});
-                this.distractors.forEach(function (item, index, array){item.move()})
+        this.all_objects = this.targets.concat(this.distractors);
+    }
+    move_balls(){
+        // function used to display all balls
+        if (!this.frozen){
+            this.all_objects.forEach(function (item){item.move()});
+        }
+    }
+    details(){
+        console.log(Object.keys(this))
+    }
+    change_color(){
+        this.all_objects.forEach(function (item){item.color = 'yellow'});
+    }
+    display_balls(){
+        // function used to display all balls
+        this.all_objects.forEach(function (item){item.display()});
+    }
+    check_collisions(){
+        for(let i =0; i< this.all_objects.length; i++){
+            for (let j=i ; j< this.all_objects.length; j++){
+                this.all_objects[i].contact(this.all_objects[j]);
             }
-        };
-        this.details = function(){
-            console.log(Object.keys(this))
-        };
-        this.change_color = function(){
-            this.targets.forEach(function (item, index, array){item.color = 'grey'});
-            this.distractors.forEach(function (item, index, array){item.color = 'grey'})
-        };
+        }
     }
 }
 
 function setup(){
   createCanvas(windowWidth, windowHeight, WEBGL);
-  ball = new Ball('yellow', 100, 1);
   app = new App(5, 5, 'yellow', 'red')
 }
 
@@ -92,6 +120,7 @@ function draw(){
     //ball.display();
     //ball.move();
     app.change_color();
+    app.check_collisions();
     app.display_balls();
     app.move_balls();
 }
