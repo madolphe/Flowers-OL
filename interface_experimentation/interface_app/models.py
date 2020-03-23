3d5076c42d117f192f97fc06b99fef8fc604f7cf
madolphe <madolphe@ensc.fr>
2020-03-24 00:04:07 +0100

    project works and is connected to db

--- interface_experimentation/interface_app/models.py
+++ interface_experimentation/interface_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Participant(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.TextField()
+    date = models.DateTimeField(default=timezone.now, verbose_name="Inscription Date")
+
+    class Meta:
+        verbose_name = 'Participant'
+        ordering = ['date']
+
+
+class Episode(models.Model):
+    date = models.DateTimeField(default=timezone.now, verbose_name="Date")
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    score = models.IntegerField()
--- interface_experimentation/interface_experimentation/settings.py
+++ interface_experimentation/interface_experimentation/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'interface_experimentation.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  'default': {
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': 'django-db',
+      'USER': 'root',
+      'PASSWORD': 'root',
+      'HOST': '127.0.0.1',
+      'PORT': '3306',
     }
 }
 
