479307053012ec8538176908835d14f58b686e0b
alexten <tenalexander1991@gmail.com>
2020-05-26 22:24:38 +0200

    Add models for administration

--- mot_project/interface_app/admin.py
+++ mot_project/interface_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models here
+admin.site.register(DynamicProps)
+admin.site.register(QBank)
+admin.site.register(Responses)
