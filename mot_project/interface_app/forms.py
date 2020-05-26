d7355a8246fab652eb495d4e0b606c8001343695
alexten <tenalexander1991@gmail.com>
2020-05-26 22:26:27 +0200

    Create a post session form

--- mot_project/interface_app/forms.py
+++ mot_project/interface_app/forms.py
@@ -1,8 +1,11 @@
 from django import forms
-from .models import ParticipantProfile
+from .models import ParticipantProfile, QBank
+from .widgets import rangeLikert
 from django.contrib.auth.models import User
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Layout, Submit, Row, Column
+from django.shortcuts import render, redirect, HttpResponse
+
 
 
 class UserForm(forms.ModelForm):
@@ -60,3 +63,26 @@ class SignInForm(forms.Form):
         super(SignInForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.add_input(Submit('submit', 'Submit'))
+
+
+class JOLDPostSessForm(forms.Form):
+    def __init__(self, questions, index, *args, **kwargs):
+        super(JOLDPostSessForm, self).__init__(*args, **kwargs)
+        self.rows = []
+        self.index = index
+        for i, q in enumerate(questions, 1):
+            if q.widget == 'range' or q.widget == 'likert':
+                self.fields[q.handle] = forms.IntegerField(
+                    widget = rangeLikert(attrs={'annotations': q.annotations.split('~'),
+                                                'min_': q.min_val,
+                                                'max_': q.max_val,
+                                                'step': q.step}),
+                    label = '{}. {}'.format(i, q.prompt))
+                self.fields[q.handle+'_validator'] = forms.BooleanField(label = '')
+                self.rows.append(
+                    Row(Column(q.handle, css_class='form-group col-10'),
+                        Column(q.handle+'_validator', css_class='form-group col-2')))
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.add_input(Submit('submit', 'Submit'))
+        self.helper.layout = Layout(*self.rows)
