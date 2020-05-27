5be22a6d88a8fbf685aefcad7cafaef5c4df6722
madolphe <madolphe@ensc.fr>
2020-05-27 09:57:53 +0200

    add light mode

--- mot_project/interface_app/static/js/app_mot/play_app_func.js
+++ mot_project/interface_app/static/js/app_mot/play_app_func.js
@@ -12,7 +12,9 @@ function play(disp_zone){
         app.display_objects(mouseX, mouseY);
         app.check_collisions();
         app.move_objects();
-        sec_task.display_task();
+        if(parameter_dict['gaming']!=0){
+            sec_task.display_task();
+        }
         display_pannel();
     }else{
         display_transition()
@@ -54,20 +56,26 @@ function start_episode(){
     button_hide_params.show();
     if(parameter_dict['episode_number']<20){
         console.log(parameter_dict);
-        //app = new MOT(3,3, Math.round(ppd*9), Math.round(ppd*3), 70, 2, 2);
         if(parameter_dict['debug']==1){
              app = new MOT(parameter_dict['n_targets'], parameter_dict['n_distractors'], Math.round(ppd*parameter_dict['angle_max']),
                   Math.round(ppd*parameter_dict['angle_min']), parameter_dict['radius'],parameter_dict['speed_min'],
                   parameter_dict['speed_max']);
         }else{
-             app = new MOT_Game(parameter_dict['n_targets'], parameter_dict['n_distractors'], Math.round(ppd*parameter_dict['angle_max']),
-                  Math.round(ppd*parameter_dict['angle_min']), parameter_dict['radius'],parameter_dict['speed_min'],
-                  parameter_dict['speed_max'], goblin_image, guard_image);
-        }
-        if(parameter_dict['secondary_task']!='none'){
-            sec_task = new Secondary_Task(leaf_image, parameter_dict['secondary_task'], parameter_dict['SRI_max']*1000,
-                parameter_dict['RSI']*1000, parameter_dict['tracking_time']*1000, parameter_dict['delta_orientation'],
-                 app.all_objects)
+            if(parameter_dict['gaming']==0){
+                console.log("no gaming mode");
+                app = new MOT_Game_Light(parameter_dict['n_targets'], parameter_dict['n_distractors'], Math.round(ppd*parameter_dict['angle_max']),
+                Math.round(ppd*parameter_dict['angle_min']), parameter_dict['radius'],parameter_dict['speed_min'],
+                    parameter_dict['speed_max'], 'green', 'red');
+            }else if(parameter_dict['gaming']==1){
+                app = new MOT_Game(parameter_dict['n_targets'], parameter_dict['n_distractors'], Math.round(ppd*parameter_dict['angle_max']),
+                Math.round(ppd*parameter_dict['angle_min']), parameter_dict['radius'],parameter_dict['speed_min'],
+                    parameter_dict['speed_max'], goblin_image, guard_image);
+                if(parameter_dict['secondary_task']!='none'){
+                    sec_task = new Secondary_Task(leaf_image, parameter_dict['secondary_task'], parameter_dict['SRI_max']*1000,
+                        parameter_dict['RSI']*1000, parameter_dict['tracking_time']*1000, parameter_dict['delta_orientation'],
+                             app.all_objects)
+                }
+            }
         }
         app.change_target_color();
         // timer(app, 2000, 2000, 10000);
@@ -93,7 +101,9 @@ function timer(app, presentation_time, fixation_time, tracking_time){
         tracking_timer = setTimeout(function(){
             // after fixation_time ms
             // app.phase change to tracking mode
-            sec_task.timer_pause();
+            if(parameter_dict['gaming']!=0) {
+                sec_task.timer_pause();
+            }
             app.phase = 'tracking';
             app.frozen = false;
             app.change_to_same_color();
@@ -119,10 +129,12 @@ function show_answer_button(){
 function answer_button_clicked(){
     button_answer.hide();
     let res = app.get_results();
-    console.log("sec_task results", sec_task.results);
     parameter_dict['nb_target_retrieved'] = res[0];
     parameter_dict['nb_distract_retrieved'] = res[1];
-    parameter_dict['sec_task_results'] = JSON.stringify(sec_task.results);
+    if(parameter_dict['gaming']==1){
+        console.log("sec_task results", sec_task.results);
+        parameter_dict['sec_task_results'] = JSON.stringify(sec_task.results);
+    }
     app.phase = 'got_response';
     app.change_to_same_color();
     app.change_target_color();
