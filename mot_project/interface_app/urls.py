56117efb627fd6670db8add32411afe2805c2efb
alexten <tenalexander1991@gmail.com>
2020-06-02 00:09:23 +0200

    Add consent form data and pointers

--- mot_project/interface_app/urls.py
+++ mot_project/interface_app/urls.py
@@ -12,11 +12,13 @@ urlpatterns = [
     path('home_user', views.home_user, name='home_user'),
     path('user_logout', views.user_logout, name='user_logout'),
     path('sign_up', views.sign_up, name='sign_up'),
+    path('consent_page', views.consent_page, name='consent_page'),
     path('app_2D', views.visual_2d_task, name='app_2D'),
     path('app_3D', views.visual_3d_task, name='app_3D'),
     path('app_MOT', views.MOT_task, name='app_MOT'),
     path('next_episode', views.next_episode, name='next_episode'),
     path('restart_episode', views.restart_episode, name='restart_episode'),
+
     ## JOLD urls
     path('joldSaveTrial_LL', views.joldSaveTrial_LL, name='JOLD_save_trial_LL'),
     path('joldEndSess', views.joldEndSess, name='JOLD_end_sess'),
