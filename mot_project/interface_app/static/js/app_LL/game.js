97d1be606e9ceb8982fbf2387cabbde2ee074563
alexten <tenalexander1991@gmail.com>
2020-06-04 23:16:54 +0200

    Remove obsolete post function

--- mot_project/interface_app/static/js/app_LL/game.js
+++ mot_project/interface_app/static/js/app_LL/game.js
@@ -854,25 +854,3 @@ function addLanderJoint(landerBody, footBody, pivLander, pivFoot, offset, side) 
     footJointDef.collideConnected = true;
     return footJointDef
 };
-
-function post(path, params, method='post') {
-
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    const form = document.createElement('form');
-    form.method = method;
-    form.action = path;
-    for (const key in params) {
-        if (params.hasOwnProperty(key)) {
-            const hiddenField = document.createElement('input');
-            hiddenField.type = 'hidden';
-            hiddenField.name = key;
-            hiddenField.value = params[key];
-
-            form.appendChild(hiddenField);
-        }
-    }
-
-    document.body.appendChild(form);
-    form.submit();
-    }
