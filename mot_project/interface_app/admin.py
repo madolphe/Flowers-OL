d1bbc8b42dfd047242ab02ada7223cd683e82aa7
Alex Ten <tenalexander1991@gmail.com>
2020-06-15 01:22:32 +0200

    Fix references to renamed models

--- mot_project/interface_app/admin.py
+++ mot_project/interface_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.sessions.models import Session
 
 # Register your models here
-admin.site.register(DynamicProps)
-admin.site.register(QBank)
+admin.site.register(Session)
+admin.site.register(StudySpec)
+admin.site.register(Question)
 admin.site.register(ParticipantProfile)
+admin.site.register(ExperimentSession)
