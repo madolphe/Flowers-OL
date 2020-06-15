bd72c3197094d0a012f4a34e683f1b3fdf3c2b03
Alex Ten <tenalexander1991@gmail.com>
2020-06-15 19:35:07 +0200

    Add more models to admin interface

--- mot_project/interface_app/admin.py
+++ mot_project/interface_app/admin.py
@@ -5,6 +5,8 @@ from django.contrib.sessions.models import Session
 # Register your models here
 admin.site.register(Session)
 admin.site.register(Study)
+admin.site.register(JOLD_LL_trial)
 admin.site.register(Question)
+admin.site.register(Answer)
 admin.site.register(ParticipantProfile)
 admin.site.register(ExperimentSession)
