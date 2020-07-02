188e441297b191b59ddf2b573bbc7e3cc7311f40
Alex Ten <tenalexander1991@gmail.com>
2020-07-02 16:07:16 +0200

    Set timezone to Paris time

--- mot_project/mot_project/settings.py
+++ mot_project/mot_project/settings.py
@@ -116,14 +116,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Europe/Paris'
 USE_I18N = True
-
-# USE_L10N = True
 DATE_INPUT_FORMATS = ['%d/%m/%Y','%d-%m-%Y', '%d.%m.%Y']
-
+# USE_L10N = True
 USE_TZ = True
 
 
