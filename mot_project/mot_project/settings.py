4d54544c2939bfc44d4e8ecb6487127a594167b9
Alex Ten <tenalexander1991@gmail.com>
2020-07-06 11:15:23 +0200

    Remove django developer panel from middleware settings

--- mot_project/mot_project/settings.py
+++ mot_project/mot_project/settings.py
@@ -58,8 +58,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'djdev_panel.middleware.DebugMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'mot_project.urls'
