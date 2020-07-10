850bd8ac703fa655979d61b767f487464b9e8b06
Alex Ten <tenalexander1991@gmail.com>
2020-07-10 11:56:18 +0200

    Display additional information for some objects viewed in admin

--- mot_project/interface_app/admin.py
+++ mot_project/interface_app/admin.py
@@ -2,12 +2,21 @@ from django.contrib import admin
 from .models import *
 from django.contrib.sessions.models import Session
 
+# Define admin classes
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'study', 'date', 'current_session', 'task_stack_csv', 'extra_json')
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'participant', 'question', 'session', 'value')
+
+
 # Register your models here
 admin.site.register(Session)
 admin.site.register(Study)
 admin.site.register(JOLD_LL_trial)
 admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(ParticipantProfile)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(ParticipantProfile, ParticipantAdmin)
 admin.site.register(ExperimentSession)
 admin.site.register(Task)
