f5b111751e7d5c1fa1ca26d15f250ae170c59867
madolphe <madolphe@ensc.Fr>
2021-03-29 23:36:09 +0200

    Add exit view to demo-task in order to tests new home behaviour.
    Add new view in demo-app with its related url in demo_app/urls.py 
    Also update demoData.json for tests purposes; change exit view.

--- flowers-ol/demo_app/urls.py
+++ flowers-ol/demo_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('demo_questionnaire', views.demo_questionnaire, name='demo_questionnaire'),
+    path('exit_demo_task', views.exit_demo_task, name='exit_demo_task'),
 ]
