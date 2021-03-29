a3afe22612dd7fa2014d4b1751a835441df8fc78
Alex Ten <tenalexander1991@gmail.com>
2021-03-29 20:42:42 +0200

    Remove obsolete forms

--- flowers-ol/manager_app/forms.py
+++ flowers-ol/manager_app/forms.py
@@ -7,59 +7,6 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import login
 
 
-class UserForm(forms.ModelForm):
-    """
-    Class to generate user form : [CreateUserForm already exists and could be overwritten]
-    """
-    username = forms.CharField(label=_("Nom d'utilisateur"),
-                               widget=forms.TextInput(attrs={'placeholder': _("Nom d'utilisateur")}))
-    password = forms.CharField(label=_('Mot de passe'),
-                               widget=forms.PasswordInput(attrs={'placeholder': _('Mot de passe')}))
-    first_name = forms.CharField(label='Prénom', widget=forms.TextInput(attrs={'placeholder': 'Prénom'}))
-    last_name = forms.CharField(label='Nom de famille', widget=forms.TextInput(attrs={'placeholder': 'Nom de famille'}))
-    email = forms.CharField(label='E-mail', widget=forms.TextInput(attrs={'placeholder': 'E-mail'}))
-    field_order = ['username', 'password', 'first_name', 'last_name', 'email']
-
-    def __init__(self, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_tag = False
-
-    class Meta:
-        model = User
-        exclude = ['groups', 'user_permissions', 'is_staff', 'is_active', 'is_superuser', 'last_login', 'date_joined']
-
-
-class ParticipantProfileForm(forms.ModelForm):
-    """
-    Class to generate a form used to register a participant.
-    """
-    def __init__(self, *args, **kwargs):
-        super(ParticipantProfileForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', 'Valider'))
-        self.helper.form_tag = False
-        self.fields['birth_date'].label = _('Date de naissance')
-        self.fields['remind'].label = _('Rappelez-moi par e-mail de compléter les tâches pour l\'étude')
-
-    class Meta:
-        model = ParticipantProfile
-        fields = ['birth_date', 'study', 'remind']
-        widgets = {'study': forms.HiddenInput(),
-                   'birth_date': forms.DateInput(attrs={'type': 'date'})
-                   }
-
-    def save_profile(self, user):
-        """
-        Method to link participant profile and django user
-        :param user:
-        :return:
-        """
-        participant_profile = super().save(commit=False)
-        participant_profile.user = user
-        super().save(commit=True)
-
-
 class SignInForm(forms.Form):
     """
     Class to generate a form for logging page.
