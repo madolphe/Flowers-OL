dacc1020c91a26d8ec510e9588bd7ec5fc8b5bd4
Maxime <maxime.balan@gmail.com>
2021-03-31 15:45:03 +0200

    Rename jold_app as jold_ll_app in settings and url

--- flowers-ol/flowers-ol/settings.py
+++ flowers-ol/flowers-ol/settings.py
@@ -55,7 +55,7 @@ USER_APPS = [
     # This is the list of user-defined apps
     'manager_app',
     'survey_app',
-    'jold_app'
+    'jold_ll_app'
 ]
 INSTALLED_APPS += USER_APPS  # append USER_APPS to list of INSTALLED_APPS
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
