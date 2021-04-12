5a02f61266b9813d6bf3272e37da4f08c73465d1
Alex Ten <tenalexander1991@gmail.com>
2021-04-12 21:02:34 +0200

    Update ConsentForm class to request only comprehension and agreement

--- flowers-ol/jold_ll_app/forms.py
+++ flowers-ol/jold_ll_app/forms.py
@@ -3,17 +3,16 @@ from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import *
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Div, HTML
-from django.utils.translation import gettext as _
 
 
 class ConsentForm(forms.Form):
-    understood = forms.BooleanField(label=_("J'ai lu et compris les termes de cette étude"), required=True)
-    agreed = forms.BooleanField(label=_("I understand that by checking this box I provide my informed consent to participate in the study"), required=True)
-    request_reminder = forms.BooleanField(label=_('I would like to be reminded about further sessions 1 day before'), required=False)
-    email = forms.EmailField(required=False)
+    understood = forms.BooleanField(label=_("En cochant cette case, vous reconnaissez avoir lu et compris les informations fournies sur l'étude."), required=True)
+    agreed = forms.BooleanField(label=_("En cochant cette case, vous donnez votre consentement éclairé à participer à l'étude."), required=True)
+    request_reminder = forms.BooleanField(label=_('En cochant cette case, vous acceptez de tecevoir un rappel 1 jour avant la prochaine session (optionel).'), required=False)
+    email = forms.EmailField(label=_('À quelle adresse souhaitez-vous recevoir le(s) rappel(s) ? Indiquez votre adresse électronique ci-dessous (optionel)'), required=False)
     fields = ['understood', 'agreed']
 
     def __init__(self, *args, **kwargs):
         super(ConsentForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', _('Valider')))
+        self.helper.add_input(Submit('submit', _('Soumettre le consentement éclairé')))
