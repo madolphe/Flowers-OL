18f544201ab5ae31e2ba692aed1beead036285f0
madolphe <madolphe@ensc.Fr>
2021-05-26 12:18:09 +0200

    Remove cursor during taskswitch

--- flowers-ol/mot_app/static/js/app_taskswitch_task/timemanager.js
+++ flowers-ol/mot_app/static/js/app_taskswitch_task/timemanager.js
@@ -91,13 +91,16 @@ class TimeManager{
         if (flag_practice==true){
           this.scene = this.tutorial_end;
           button_start.show();
+          remove_hide_cursor_class();
         }else{
           if (flag_break==true){
             this.scene = this.scene_break;
             button_start.show();
+            remove_hide_cursor_class();
           }else{
             this.scene = this.scene_end;
             button_end.show();
+            remove_hide_cursor_class();
           }
         }
       }else{
