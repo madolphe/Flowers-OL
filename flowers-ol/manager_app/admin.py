d02439f2fa5b88382366ee0a65ac2193d5b4ad5f
madolphe <madolphe@ensc.Fr>
2021-10-18 08:53:33 +0200

    Clean branch + change label of signup form

--- flowers-ol/manager_app/admin.py
+++ flowers-ol/manager_app/admin.py
@@ -9,15 +9,9 @@ class ParticipantAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ('user', 'origin_timestamp', 'session_stack_csv', 'current_session', 'task_stack', 'extra_json')
     actions = ["export_as_csv"]
 
-class EpisodeAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'participant', 'get_results', 'n_targets', 'n_distractors', 'probe_time',
-                    'tracking_time', 'speed_max')
-
 # Register your models here
 admin.site.register(Session)
 admin.site.register(Study)
 admin.site.register(ParticipantProfile, ParticipantAdmin)
 admin.site.register(ExperimentSession)
 admin.site.register(Task)
-admin.site.register(Episode)
-
--- flowers-ol/manager_app/views.py
+++ flowers-ol/manager_app/views.py
@@ -16,10 +16,12 @@ from .forms import SignInForm, SignUpForm
 
 
 def login_page(request, study=''):
-    # If study key exists in request.session dict, get its values. This works when user requests the login page without specifying study extension and when the user already has a session
+    # If study key exists in request.session dict, get its values. This works when user requests the login page without
+    # specifying study extension and when the user already has a session
     if 'study' in request.session:
         study = request.session.get('study')
-    # If user requests login page with a specific study extension, e.g. http://web-address.fr/study=name_of_study, the name_of_study will be assigned to the study variable
+    # If user requests login page with a specific study extension, e.g. http://web-address.fr/study=name_of_study,
+    # the name_of_study will be assigned to the study variable
     # This also overwrites study name currently stored in user's session
     if 'study' in request.GET.dict():
         study = request.GET.dict().get('study')
@@ -27,7 +29,7 @@ def login_page(request, study=''):
     valid_study_title = bool(Study.objects.filter(name=study).count())
     if valid_study_title:
         # Normally, a participant has link to only one study. Thus, this should only be performed once
-        request.session['study'] = study # store 'study' extension only once per session
+        request.session['study'] = study  # store 'study' extension only once per session
     error = False
     form_sign_in = SignInForm(request.POST or None)
     if form_sign_in.is_valid():
@@ -87,7 +89,7 @@ def home(request):
 
     # If participant has a session assigned, set request.session.active_session to True
     if participant.current_session:
-         request.session['active_session'] = json.dumps(True)
+        request.session['active_session'] = json.dumps(True)
 
     # If there are any messages, add them to django messages see https://docs.djangoproject.com/en/dev/ref/contrib/messages/
     if 'messages' in request.session:
@@ -106,17 +108,20 @@ def start_task(request):
 @login_required
 def off_session_page(request, case):
     participant = request.user.participantprofile
-    
+
     # Get next session if session stack is not empty, otherwise assign None to session
     session = None  # assigning None might be redundant, which is a good thing here!
     if participant.session_stack_peek():
         if case == 'early':
-            reason = _('La session ne peut pas être lancée parce qu\'il ne s\'est pas écoulé suffisamment de temps depuis votre dernière session.')
+            reason = _(
+                'La session ne peut pas être lancée parce qu\'il ne s\'est pas écoulé suffisamment de temps depuis votre dernière session.')
             instructions = _('Dans ce cas, revenez plus tard. Votre prochaine session')
             details = participant.get_next_session_info()
         elif case == 'late':
-            reason = _('Vous avez manqué une des sessions prévues. Si vous avez commencé une session plus tôt mais que vous ne l\'avez pas terminée à la date limite, la session est considérée comme manquée.')
-            instructions = _('Dans ce cas, vous n\'avez pas respecté le programme et vous ne pouvez pas poursuivre l\'expérience. Merci de participer à notre recherche.')
+            reason = _(
+                'Vous avez manqué une des sessions prévues. Si vous avez commencé une session plus tôt mais que vous ne l\'avez pas terminée à la date limite, la session est considérée comme manquée.')
+            instructions = _(
+                'Dans ce cas, vous n\'avez pas respecté le programme et vous ne pouvez pas poursuivre l\'expérience. Merci de participer à notre recherche.')
             details = None
         return render(request, 'off_session_page.html', {'CONTEXT': {
             'reason': reason, 'instructions': instructions, 'details': details}})
@@ -145,8 +150,8 @@ def thanks_page(request):
             if not participant.sessions.get(pk=next_session_pk).wait:
                 # If session can start immediately
                 text = _('Votre entraînement n\'est pas fini pour aujourd\'hui, il vous reste une ' \
-                       'session à effectuer durant la journée! Si vous voulez continuer immédiatement c\'est possible:'\
-                       ' Déconnectez vous, reconnectez vous et recommencez !')
+                         'session à effectuer durant la journée! Si vous voulez continuer immédiatement c\'est possible:' \
+                         ' Déconnectez vous, reconnectez vous et recommencez !')
             else:
                 # If session has wait time
                 next_session_info = participant.get_next_session_info()
@@ -154,7 +159,8 @@ def thanks_page(request):
         else:
             text = _('Nous vous attendons la prochaine fois.')
         if participant.excluded:
-            text = _('Votre participation a été interrompue. Vous pouvez nous contacter pour plus d\'informations. Merci pour votre temps !')
+            text = _(
+                'Votre participation a été interrompue. Vous pouvez nous contacter pour plus d\'informations. Merci pour votre temps !')
     else:
         # If participant has no sessions left
         heading = _('L\'étude est terminée')
@@ -206,35 +212,35 @@ def fork_super(request):
 
 @user_passes_test(lambda u: u.is_superuser)
 def home_super(request):
-        p = request.user.participantprofile
-        if not p.set_current_session():
-            return redirect(reverse(thanks_page))
+    p = request.user.participantprofile
+    if not p.set_current_session():
+        return redirect(reverse(thanks_page))
 
-        time_stamp = p.last_session_timestamp.strftime('%d %b %Y (%H:%M:%S)') if p.last_session_timestamp else None
-        valid_period = p.current_session.get_valid_period(p.ref_timestamp, string_format='%d %b %Y (%H:%M:%S)')
+    time_stamp = p.last_session_timestamp.strftime('%d %b %Y (%H:%M:%S)') if p.last_session_timestamp else None
+    valid_period = p.current_session.get_valid_period(p.ref_timestamp, string_format='%d %b %Y (%H:%M:%S)')
 
-        now = timezone.now().strftime('%d %b %Y (%H:%M:%S)')
-        now_is, destination = f'good time [{now}]', 'start_task'
-        if p.current_session.in_future(p.ref_timestamp):
-            now_is, destination = f'too early [{now}]', 'off_session_page'
-        elif p.current_session.in_past(p.ref_timestamp):
-            now_is = f'too late [{now}]'
-            if p.current_session.required:
-                destination = 'thanks_page'
-            else:
-                destination = 'end_session'
-        
-        return render(request, 'home_super.html', 
-            {
-                'CONTEXT': {
-                    'p': p,
-                    'time_stamp': time_stamp,
-                    'valid_period': valid_period,
-                    'now_is': now_is,
-                    'destination': destination
-                }
-            }
-        )
+    now = timezone.now().strftime('%d %b %Y (%H:%M:%S)')
+    now_is, destination = f'good time [{now}]', 'start_task'
+    if p.current_session.in_future(p.ref_timestamp):
+        now_is, destination = f'too early [{now}]', 'off_session_page'
+    elif p.current_session.in_past(p.ref_timestamp):
+        now_is = f'too late [{now}]'
+        if p.current_session.required:
+            destination = 'thanks_page'
+        else:
+            destination = 'end_session'
+
+    return render(request, 'home_super.html',
+                  {
+                      'CONTEXT': {
+                          'p': p,
+                          'time_stamp': time_stamp,
+                          'valid_period': valid_period,
+                          'now_is': now_is,
+                          'destination': destination
+                      }
+                  }
+                  )
 
 
 @user_passes_test(lambda u: u.is_superuser)
