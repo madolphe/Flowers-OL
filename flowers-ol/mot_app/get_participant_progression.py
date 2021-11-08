f2db207fdb662ca85cfc82beef87df0b3f18a440
madolphe <madolphe@ensc.Fr>
2021-11-08 11:24:53 +0100

    Add dashboard

---
+++ flowers-ol/mot_app/get_participant_progression.py
@@ -0,0 +1,140 @@
+import importlib
+import os
+import django
+import pandas as pd
+from datetime import datetime
+import pytz
+
+# Connection to flowers-DB:
+# flowers_ol = importlib.import_module("flowers-ol.settings")
+#
+# os.environ.setdefault(
+#     "DJANGO_SETTINGS_MODULE",
+#     "flowers-ol.settings"
+# )
+# django.setup()
+
+from .models import Episode, CognitiveResult
+from manager_app.models import ParticipantProfile
+
+
+def get_mean_idle_time(participant):
+    all_episodes = Episode.objects.all().filter(participant__username=participant)
+    sessions = {}
+    for episode in all_episodes:
+        if episode.id_session not in sessions:
+            sessions[episode.id_session] = []
+        sessions[episode.id_session].append(episode)
+    check_episode_date()
+    idle_times = {}
+    for session_key, session_content in sessions.items():
+        shift_session = session_content[1::]
+        idle_times[session_key] = []
+        for episode, next_episode in zip(session_content, shift_session):
+            if next_episode:
+                delta = next_episode.date - episode.date
+                episode_length = episode.tracking_time + episode.presentation_time + episode.fixation_time + episode.probe_time
+                idle_times[session_key].append((delta.total_seconds() - episode_length))
+        print(sum(idle_times[session_key]) / len(idle_times[session_key]))
+
+
+def check_episode_date():
+    pass
+
+
+def get_exp_status(study):
+    all_participants = ParticipantProfile.objects.all().filter(study__name=study)
+    nb_participants = len(all_participants)
+    nb_cog_assessment_list = [(participant, get_nb_cog_assessment_for_participant(participant)) for participant in
+                              all_participants]
+    nb_participants_in = sum([nb == 8 for (participant, nb) in nb_cog_assessment_list])
+    zpdes_participants, baseline_participants, none_participants = get_groups(all_participants)
+    nb_baseline, nb_zpdes = len(baseline_participants), len(zpdes_participants)
+    descriptive_dict = {'zpdes': get_progression(zpdes_participants),
+                        'baseline': get_progression(baseline_participants),
+                        'cog': get_progression(none_participants)}
+    return nb_participants, nb_participants_in, nb_baseline, nb_zpdes, descriptive_dict
+
+
+def get_nb_cog_assessment_for_participant(participant):
+    particpants_cog_results = CognitiveResult.objects.all().filter(participant=participant)
+    return len(particpants_cog_results)
+
+
+def get_groups(all_participants):
+    zpdes_participants, baseline_participants, none_participants = [], [], []
+    for participant in all_participants:
+        if "condition" in participant.extra_json:
+            if participant.extra_json["condition"] == "zpdes":
+                zpdes_participants.append(participant)
+            else:
+                baseline_participants.append(participant)
+        else:
+            none_participants.append(participant)
+    return zpdes_participants, baseline_participants, none_participants
+
+
+def get_progression(participants_list):
+    descriptive_dict = {}
+    for participant in participants_list:
+        pk = participant.session_stack_peek()
+        if not pk:
+            participant_progression = [1 for i in range(10)]
+        else:
+            next_session = participant.sessions.get(pk=pk)
+            participant_progression = [1 for i in range(next_session.index)]
+            if bool(participant.current_session):
+                if len(participant_progression) == 0:
+                    participant.last_session_timestamp = participant.origin_timestamp
+                # participant has connected but didnt finished session of the day
+                if (get_time_since_last_session(participant) - 1) < 3:
+                    participant_progression.append(-1)
+                else:
+                    participant_progression.append(-2)
+            else:
+                # participant has not reconnected:
+                s = participant.sessions.get(pk=participant.session_stack_peek())
+                if s.in_future(participant.ref_timestamp):
+                    participant_progression.append(0)
+                else:
+                    # session is available but has not started it yet --> -1 or -2
+                    participant.current_session = s
+                    if get_time_since_last_session(participant) < 3 + participant.current_session.wait['days']:
+                        participant_progression.append(-1)
+                    else:
+                        participant_progression.append(-2)
+        if 'condition' in participant.extra_json:
+            cond = participant.extra_json['condition']
+            nb_episode = get_number_episode_played(participant)
+            idle_time = get_mean_idle_time(participant) / 1000
+        else:
+            cond = 'no_group'
+            nb_episode = 0
+            idle_time = 0
+        none_blocks = [0 for i in range(10-len(participant_progression))]
+        descriptive_dict[participant.user.username] = (cond, participant_progression, none_blocks, nb_episode, idle_time)
+    return descriptive_dict
+
+
+def get_number_episode_played(participant):
+    return len(Episode.objects.all().filter(participant=participant.user))
+
+
+def get_mean_idle_time(participant):
+    episodes = Episode.objects.all().filter(participant=participant.user)
+    mean_idles = [episode.idle_time for episode in episodes]
+    return sum(mean_idles) / len(mean_idles)
+
+
+def get_time_since_last_session(participant):
+    """
+    Return -1 if available but not finished within 3 days
+    Return -2 if available but not finished in more than 3 days
+    """
+    return (datetime.now(pytz.utc) - participant.last_session_timestamp).days
+
+
+if __name__ == '__main__':
+    # get_mean_idle_time("v1_ubx")
+    nb_participants, nb_participants_in, descriptive_dict = get_exp_status("v1_ubx")
+    print(nb_participants, nb_participants_in, descriptive_dict)
