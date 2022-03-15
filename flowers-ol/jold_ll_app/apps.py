627d613040ede53774c89045c262724b8b46273f
madolphe <madolphe@ensc.Fr>
2022-03-15 16:29:07 +0100

    update gitignore + delete apps and push them into submodules

--- flowers-ol/jold_ll_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JoldAppConfig(AppConfig):
-    name = 'jold_ll_app'
--- flowers-ol/mot_app/translation.py
+++
@@ -1,9 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from .models import CognitiveTask
-
-
-class CognitiveTaskTranslationOptions(TranslationOptions):
-    fields = ['name']
-
-
-translator.register(CognitiveTask, CognitiveTaskTranslationOptions)
