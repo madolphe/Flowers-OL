d32a5934ad779663090b30e0b443be434c606461
Maxime ADOLPHE <madolphe@ensc.fr>
2024-11-15 21:37:32 +0100

    update main to only rely on the manager app i.e delete useless urls to other apps in urls.py

--- flowers-ol/flowers-ol/urls.py
+++ flowers-ol/flowers-ol/urls.py
@@ -21,8 +21,4 @@ from django.views.i18n import JavaScriptCatalog
 urlpatterns = [
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('admin/', admin.site.urls),
-    path('', include('manager_app.urls')),
-    path('', include('jold_ll_app.urls')),
-    path('', include('survey_app.urls')),
-    path('', include('mot_app.urls'))
-]
+    path('', include('manager_app.urls'))]
